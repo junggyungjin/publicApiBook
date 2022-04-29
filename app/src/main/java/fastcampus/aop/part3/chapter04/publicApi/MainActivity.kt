72e7dc3b942bb28abe9385bd7ea08ce3c331bdb1
junggyungjin <junggyungjin@svcorps.dev>
2022-04-29 11:05:37 +0900

    editText 검색 기능 추가

--- app/src/main/java/fastcampus/aop/part3/chapter04/publicApi/MainActivity.kt
+++ app/src/main/java/fastcampus/aop/part3/chapter04/publicApi/MainActivity.kt
@@ -3,6 +3,8 @@ package fastcampus.aop.part3.chapter04.publicApi
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.KeyEvent
+import android.view.MotionEvent
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.gson.GsonBuilder
 import fastcampus.aop.part3.chapter04.publicApi.adapter.BookAdapter
@@ -10,6 +12,7 @@ import fastcampus.aop.part3.chapter04.publicApi.api.BookService
 import fastcampus.aop.part3.chapter04.publicApi.databinding.ActivityMainBinding
 import fastcampus.aop.part3.chapter04.publicApi.model.BestSellerDto
 import fastcampus.aop.part3.chapter04.publicApi.model.Book
+import fastcampus.aop.part3.chapter04.publicApi.model.SearchBookDto
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -20,6 +23,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var adapter: BookAdapter
+    private lateinit var bookService: BookService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,9 +39,9 @@ class MainActivity : AppCompatActivity() {
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
-        val bookService = retrofit.create(BookService::class.java)
+        bookService = retrofit.create(BookService::class.java)
 
-        bookService.getBestSellerBooks("BB916F3DC6A4EB788A5E7C5F0A111F8D14A587DB79B0292E1BA671991700EC97")
+        bookService.getBestSellerBooks(getString(R.string.interParkAPIKey))
             .enqueue(object: Callback<BestSellerDto> {
                 override fun onResponse(
                     call: Call<BestSellerDto>,
@@ -64,9 +68,39 @@ class MainActivity : AppCompatActivity() {
                 }
 
             })
+
+        binding.searchEditText.setOnKeyListener { v, keyCode, keyEvent ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER && keyEvent.action == MotionEvent.ACTION_DOWN) {
+                search(binding.searchEditText.text.toString())
+                return@setOnKeyListener true // return@setOnKeyListener true의 의미 : 내가 실제로 이 이벤트를 처리했음을 의미
+            }
+            return@setOnKeyListener false // 처리가 안된 if문에 걸리지 않은 나머지는 처리가 안되었다.
+        }
     }
 
-    fun initBookRecyclearView() {
+    private fun search(keyword: String) {
+        bookService.getBookByName(getString(R.string.interParkAPIKey), keyword)
+            .enqueue(object: Callback<SearchBookDto> {
+                override fun onResponse(
+                    call: Call<SearchBookDto>,
+                    response: Response<SearchBookDto>
+                ) {
+                    if (response.isSuccessful.not()) {
+                        return
+                    }
+
+                    adapter.submitList(response.body()?.books.orEmpty()) //submitList() 함수를 쓰게되면 리스트가 이걸로 체인지가 된다 TODO 공부 필요
+
+
+                }
+
+                override fun onFailure(call: Call<SearchBookDto>, t: Throwable) {
+                }
+
+            })
+    }
+
+    private fun initBookRecyclearView() {
         adapter = BookAdapter()
         binding.bookRecyclerView.layoutManager = LinearLayoutManager(this)
         binding.bookRecyclerView.adapter = adapter
